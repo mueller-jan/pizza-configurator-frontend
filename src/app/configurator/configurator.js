4744940141faa50bddbe506bb79052bfb16557dd
Jan <jmueller02@stud.hs-bremen.de>
2016-05-10 14:59:00 +0200

    generate random pizza (two-way-binding of checkboxes stops working after second randomization)

--- src/app/configurator/configurator.js
+++ src/app/configurator/configurator.js
@@ -1,5 +1,5 @@
 angular.module('app.configurator',
-    ['ui.router', 'checklist-model'])
+    ['ui.router', 'checklist-model', 'ui.bootstrap'])
 
     .config(function config($stateProvider) {
         $stateProvider.state('configurator', {
@@ -17,9 +17,21 @@ angular.module('app.configurator',
 
     .controller('configuratorCtrl', function ConfiguratorController($scope) {
         $scope.selectedIngredients = [];
+
         $scope.ingredients = [
             {name: 'Pineapple', imagePath: './assets/pineapple.png'},
             {name: 'Swiss Cheese', imagePath: './assets/swisscheese.png'},
             {name: 'Mango', imagePath: './assets/mango.png'}
         ];
+
+        $scope.randomize = function() {
+            $scope.selectedIngredients = [];
+
+            for (var i = 0; i < $scope.ingredients.length; i++) {
+                if (Math.random() < 0.5) {
+                    $scope.selectedIngredients.push($scope.ingredients[i])
+                }
+            }
+        };
+
     });

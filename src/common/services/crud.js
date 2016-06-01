c91bc798192b6b608f21b7b7bbd0fe147527c1f9
mueller-jan <mueller.jan.87@gmail.com>
2016-06-01 12:22:46 +0200

    Updated endpoints

--- src/common/services/crud.js
+++ src/common/services/crud.js
@@ -2,11 +2,11 @@ angular.module('services.crud', ['app.config'])
     .factory('CrudService', function ($http, API_URL) {
         return {
             createUser: function (user) {
-                return $http.post(API_URL + '/user', user, {headers: {'Content-Type': 'application/json'}});
+                return $http.post(API_URL + '/users', user, {headers: {'Content-Type': 'application/json'}});
             },
 
             createAddress: function (userId, address) {
-                return $http.post(API_URL + '/user/' + userId + '/address', address, {headers: {'Content-Type': 'application/json'}});
+                return $http.post(API_URL + '/users/' + userId + '/addresses', address, {headers: {'Content-Type': 'application/json'}});
             }
         }
     });
